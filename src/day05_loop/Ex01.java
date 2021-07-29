100d8da1a0e2948afd3145c6a630ec8f299dd063
Hoon <dkhj0615@naver.com>
2021-07-29 11:11:17 +0900

    for 반복문이랑 Quiz까지

--- src/day05_loop/Ex01.java
+++ src/day05_loop/Ex01.java
@@ -1,5 +1,33 @@
 package day05_loop;
 
-public class Ex01 {
+import java.util.Scanner;
 
+public class Ex01 {
+	public static void main(String[] args) {
+		/*
+		 for문은 ; 개수가 무조건 2개가 들어와야 한다
+		 초기값 조건식 증감식은 없어도 상관없다
+		 for(초기값 ; 조건식 ; 증감식{
+		 	종속문장
+		 }
+		 */
+		int sum = 0, cnt = 1;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		sum += cnt++;
+		System.out.println(sum);
+		System.out.print(cnt);
+		sum = 0;
+		for(cnt=1;cnt<=10;cnt++) {
+			sum+=cnt;
+		}
+		System.out.println("sum : "+sum);
+	}
 }
